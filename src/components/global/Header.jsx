c17a94671a8f3bd2e32cb342786714a18fe6d09c
João <joaopedromari28@gmail.com>
2024-11-29 13:00:24 -0300

    feat: Criado component Header

---
+++ src/components/global/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../../assets/imgs/logoRoomOM.png";
+import ButtonCadastro from "../UI/ButtonCadastro";
+import ButtonLogin from "../UI/ButtonLogin";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex justify-between py-5 lg:px-20 px-2">
+        <div className="flex items-center gap-2">
+          <img className="w-[30px]" src={logo} alt="Logo do RoomOM" />
+          <h3 className="text-lg font-bold font-main text-white uppercase">
+            Room.OM
+          </h3>
+        </div>
+
+        <div className="flex lg:gap-5 gap-2">
+          <ButtonCadastro redirect="/cadastro">Cadastre-se</ButtonCadastro>
+          <ButtonLogin redirect="/logar">Entrar</ButtonLogin>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
